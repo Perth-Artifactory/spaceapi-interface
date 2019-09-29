02380d3449c5c4d33ea2ee173308d6c49722a435
Blake Samuels <16077190+tazard@users.noreply.github.com>
2019-09-29 21:03:48 +0800

    Configure with json config file supplied on commandline.

--- door_timer_spaces_api.py
+++ door_timer_spaces_api.py
@@ -2,20 +2,24 @@ import paho.mqtt.client as mqtt  # pip install paho-mqtt
 import time
 import json
 import os
+import argparse
 
-MQTT_SERVER_IP = "util"
-# SPACEAPI_TEMPLATE = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.template.json"
-# SPACEAPI_TEMP = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.json.tmp"
-# SPACEAPI_OUT = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.json"
-
-SPACEAPI_TEMPLATE = r"/home/spaceapi/spaceapi.template.json"
-SPACEAPI_TEMP = r"/home/spaceapi/spaceapi.json.tmp"
-SPACEAPI_OUT = r"/var/www/space/spaceapi.json"
+# MQTT_SERVER_IP = "util"
+# # SPACEAPI_TEMPLATE = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.template.json"
+# # SPACEAPI_TEMP = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.json.tmp"
+# # SPACEAPI_OUT = r"C:\Code\python\artifactory_spaces_api_interface\spaceapi.json"
+#
+# SPACEAPI_TEMPLATE = r"/home/spaceapi/spaceapi.template.json"
+# SPACEAPI_TEMP = r"/home/spaceapi/spaceapi.json.tmp"
+# SPACEAPI_OUT = r"/var/www/space/spaceapi.json"
 
 ENCODING = "utf-8"
 
 MQTT_TOPIC_STATUS = "status/arduino_artimer"
 MQTT_TOPIC_TIME_REMAIN = "sensors/arduino_artimer/time_remaining"
+
+config = {}
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -33,7 +37,7 @@ def on_message(client, userdata, msg):
     try:
         payload = msg.payload.decode(ENCODING)
         topic = msg.topic
-        print("{} {}".format(topic, payload))
+        print("Received '{}' '{}'".format(topic, payload))
         if topic == MQTT_TOPIC_TIME_REMAIN:
             time_remaining = float(payload)
             update_spaceapi(time_remaining=time_remaining)
@@ -42,13 +46,15 @@ def on_message(client, userdata, msg):
 
 
 def update_spaceapi(time_remaining):
+    global config
+
     open_state = (time_remaining > 0)
     if open_state:
         message = "Open for {} hours (by Door Timer)".format(time_remaining)
     else:
         message = 'Closed (by Door Timer)'
 
-    with open(SPACEAPI_TEMPLATE) as f:
+    with open(config['spaceapi_template']) as f:
         j = json.load(f)
 
     # Update the json contents
@@ -58,26 +64,38 @@ def update_spaceapi(time_remaining):
     state['lastchange'] = time.time()
     state['message'] = message
 
-    with open(SPACEAPI_TEMP, 'w') as f:
+    with open(config['spaceapi_temp'], 'w') as f:
         json.dump(j, f, sort_keys=True, indent=4, separators=(',', ': '))
 
-    os.replace(SPACEAPI_TEMP, SPACEAPI_OUT)
+    os.replace(config['spaceapi_temp'], config['spaceapi_out'])
 
-    print("Updated: {}".format(SPACEAPI_OUT))
+    print("Updated: {}".format(config['spaceapi_out']))
+
+
+def read_config(path):
+    with open(path) as f:
+        j = json.load(f)
+    return j
 
 
 def main():
+    arg_parser = argparse.ArgumentParser(description="SpacesAPI interface")
+    arg_parser.add_argument('config', help='path to config json file')
+    args = arg_parser.parse_args()
+
+    global config
+    config = read_config(args.config)
+    print(f"Config: {config}")
+
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
 
-    client.connect(MQTT_SERVER_IP, 1883, 60)
+    client.connect(config["mqtt_server"], 1883, 60)
 
     client.loop_start()
 
     while True:
-        # temperature = sensor.blocking_read()
-        # client.publish("paho/temperature", temperature)
         pass
 
 
